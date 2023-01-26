729c30a9a0afebb9081b865268fe838da9daac1b
Aymsep <71574687+Aymsep@users.noreply.github.com>
2023-01-26 12:18:50 +0100

    commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function ln(sen){
-    let sentence_length = sen.length;
-    let word_num = Array.from(sen.split(" ")).length
-    let vowel = Array.from(sen)
-    let vowel_total = 0
-    let count_vowel = vowel.forEach(item => {
-        let arr = ['a','e','i','o','u','y']
-        if(arr.includes(item)){
-            vowel_total += 1 
-        }
-
-    })
-    return [sentence_length, word_num, vowel_total]
+function ln(arr1,arr2){
+    const a = new Set([...arr1,...arr2])
+    const sum = [...a].reduce((total,item)=>{
+        return total + item
+    },0)
+    console.log(a)
+    console.log(sum) 
 }
-console.log(ln('lfdjsl fldsjf flkdsjf lkjfsd') )
+ln([1,2,3,3],[1,2,3,5,4])
+
+function sm(arr1, arr2) {
+    const a = [...arr1,...arr2]
+    let set = new Set(a)
+    let nwarr = a.filter((item,index) => a.indexOf(item) !== index);
+    let sum = nwarr.reduce((sum, item) => sum + item)
+    console.log(sum)
+
+    
+}
+sm([1,2,3,4,5,1,3],[1,2,3])
